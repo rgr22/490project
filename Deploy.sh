f887fa947d0951417798c28d3101b33c57f41cd8
robin <rgr22@njit.edu>
2015-12-16 15:46:24 -0500

    Done, need to work on sesions

---
+++ Deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+tar -xvzf /home/robin/Desktop/Packages/$1.tar.gz -C /
---
+++ MovePackage.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+scp /home/robin/Desktop/Packages/$1.tar.gz robin@192.168.1.17:/home/robin/Desktop/Packages/$1.tar.gz
